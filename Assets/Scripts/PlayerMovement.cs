16fd601d9f20d6d822ca1b973fe67567dca558c9
Julian Moczko <julianmoczko@gmail.com>
2022-03-17 08:09:32 +0100

    Unity Init

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    
+    private InputHandler _input;
+    [SerializeField]
+    private float moveSpeed;
+
+    private void Awake ()
+    {
+        _input = GetComponent<InputHandler>();
+
+    }
+    void Update()
+    {
+        var targetVector = new Vector3(_input.InputVector.x, 0, _input.InputVector.y);
+
+        //Move in the direction we're aming
+        MoveTowardTarget(targetVector);
+
+
+
+        //Rotate in the direction we're traveling
+    }
+
+    private void MoveTowardTarget(Vector3 targetVector)
+    {
+        var speed = moveSpeed * Time.deltaTime;
+        transform.Translate(targetVector * speed);
+    }
+}
