325b096a6545019c10738a73eacd6983da95a6a6
Julian Moczko <julianmoczko@gmail.com>
2022-03-24 19:24:38 +0100

    Traps suggestions

--- Assets/Scripts/PlayerDeath.cs
+++ Assets/Scripts/PlayerDeath.cs
@@ -19,3 +19,12 @@ private Vector3 startPosition;
         transform.position = startPosition;
     }
 }
+// mając trapa, żeby trap mówił graczowi, że ma go zabić
+// tag obstacle nie mówi dużo, trza żeby mówił, że zabija
+// relacja ma być tylko pomiędzy trapem a graczem. Event system - trap > wejście > jeden obiekt decyduje co się dzieje
+// pobrać z other (get component) > wykonać akcję na obiekcie
+// interesuje nas tylko gracz
+// trap powinien mieć metodę a gracz info czy wszedł
+// zależności dwukierunkowe to ZUO
+// poczytać o solid (abstrakcje a nie konkretne realizacje) (Pioprek się czepia)
+// klasa pickup z pierwszego semestru
