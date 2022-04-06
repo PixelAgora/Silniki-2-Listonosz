908f7b007f82c79b891151d05805aff4e20824ca
Wolfer2035 <krzysztofglazna.amigo@gmail.com>
2022-04-06 18:40:18 +0200

    Player rotation working
    Zrobiłem skrypt w którym postać się obraca tylko poruszanie się wsadem jest relatywne do tego gdzie postać się rusza, tzn. jeśli patrzę w prawo to W sprawia że idę w prawo

--- Assets/Scripts/PlayerMouseRotation.cs
+++ Assets/Scripts/PlayerMouseRotation.cs
@@ -4,16 +4,30 @@ using UnityEngine;
 
 public class PlayerMouseRotation : MonoBehaviour
 {
+    private Camera mainCamera;
 
-
+    private void Start()
+    {
+        mainCamera = FindObjectOfType<Camera>();
+    }
     void Update()
     {
-        Vector3 mouseWorldPosition = Input.mousePosition;
+        Ray cameraRay = mainCamera.ScreenPointToRay(Input.mousePosition);
+        Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+        float rayLength;
+
+        if (groundPlane.Raycast(cameraRay, out rayLength))
+        {
+            Vector3 pointToLook = cameraRay.GetPoint(rayLength);
+
+            transform.LookAt(new Vector3(pointToLook.x, transform.position.y, pointToLook.z));
+        }
+        /*Vector3 mouseWorldPosition = Input.mousePosition;
         float angle = AngleBetweenPoints(transform.position, mouseWorldPosition);
-        transform.rotation =  Quaternion.Euler(new Vector3(0f,angle,0f));
+        transform.rotation =  Quaternion.Euler(new Vector3(0f,angle,0f));*/
     }
-    private float AngleBetweenPoints(Vector2 a, Vector2 b)
+    /*private float AngleBetweenPoints(Vector2 a, Vector2 b)
     {
         return Mathf.Atan2(a.y - b.y, a.x - b.x) * Mathf.Rad2Deg;
-    }
+    }*/
 }
