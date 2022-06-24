7c643097043351c44493b0f1fe42ed30995666ef
Julian Moczko <julianmoczko@gmail.com>
2022-06-24 16:11:11 +0200

    no errors

--- Assets/Scripts/Enemies related/CastielInteraction.cs
+++ Assets/Scripts/Enemies related/CastielInteraction.cs
@@ -1,25 +1,25 @@
-using Code.InteractionSystem;
-using UnityEngine;
-using System.Collections;
-using System.Collections.Generic;
-using TMPro;
+// using Code.InteractionSystem;
+// using UnityEngine;
+// using System.Collections;
+// using System.Collections.Generic;
+// using TMPro;
 
-namespace Code.ExampleInteractive
-{
-    public class CastielInteraction : MonoBehaviour, IInteractive
-    {
-        public TextMeshProUGUI textComponent;
-        public TextMeshProUGUI justno;
+// namespace Code.ExampleInteractive
+// {
+//     public class CastielInteraction : MonoBehaviour, IInteractive
+//     {
+//         public TextMeshProUGUI textComponent;
+//         public TextMeshProUGUI justno;
 
-        public bool IsInteracting { get; private set; }
-        public bool InteractOnHold => false;
+//         public bool IsInteracting { get; private set; }
+//         public bool InteractOnHold => false;
 
-        public void Interact(Interactor interactor)
-        {
-            if (IsInteracting){
+//         public void Interact(Interactor interactor)
+//         {
+//             if (IsInteracting){
                 
-                textComponent;
-            }
-        }
-    }
-}
+//                 textComponent;
+//             }
+//         }
+//     }
+// }
